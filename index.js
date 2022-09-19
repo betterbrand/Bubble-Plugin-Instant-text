59da58385f0fb642c44f120a7045759482277ed3
wolf <billodida420@gmail.com>
2022-09-19 11:20:01 +0300

    adding function to load schema

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const axios = require("axios");
 
 const app = express();
 
+const { UrlLoader } = require('@graphql-tools/url-loader')
+
+const { loadSchema } = require('@graphql-tools/load')
+
 app.listen(3000, function() {
     console.log("Server is listening on port 3000...");
 });
 
 app.post('/graphql', (req, res)=> {
 
-
-    console.log(req);
+    try {
+        console.log(req);
 
     const url = req.url;
 
@@ -43,7 +47,37 @@ app.post('/graphql', (req, res)=> {
         console.log(result.data)
         return res.status(200).json(result.data);
       })
+    } catch(error) {
+        return res.status(500).json({
+            'error' : error
+        })
+    }
+    
 
    
 
+})
+
+app.post('/loadSchema', async (req, res)  => {
+
+    try {
+        console.log(req);
+
+        const url = req.url;
+    
+        const schema = await loadSchema('http://localhost:3000/graphql', {
+        loaders: [new UrlLoader()]
+      })
+    
+    return res.status(200).json(
+            {
+                'schema' : schema
+            }
+        );
+    } catch(error) {
+        return res.status(500).json({
+            'error' : error
+        })
+    }
+   
 })
